48faea5b730d70097ed5c867e5dfb412aaeb3275
laxminarayana33 <laxminarayanakota8@gmail.com>
2020-06-29 12:43:06 +0530

    database added commited

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DB_URI: 'mongodb+srv://pis:pis123@lms-kva0p.mongodb.net/test?retryWrites=true&w=majority',
+  DB_URI: 'mongodb+srv://laxminarayana:narayana@cluster0-bjtrz.mongodb.net/angularbackend?retryWrites=true&w=majority',
   SECRET: 'secret',
   AWS_ACCESS_KEY_ID: 'AKIAJBCULA7ENE6OGPPQ',
   AWS_SECRET_ACCESS_KEY: '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',
